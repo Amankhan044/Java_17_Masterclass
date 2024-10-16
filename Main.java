9bafc31b01387a1821b3bdcaa37edccac5aa1834
Amankhan044 <amankhan06a@gmail.com>
2024-10-16 18:11:41 +0500

    Challenge-Completed

--- Main.java
+++ Main.java
@@ -1,10 +1,14 @@
 
 import ControlFlow.*;
 import OopInheritance.*;
+import OopInheritance.InheritanceChallenge.*;
 
 @SuppressWarnings("unused")
 public class Main {
     public static void main(String[] args) {
-
+        HourlyEmployee tim = new HourlyEmployee("Tim", "11/11/1995", 0, "11/11/2024", 15);
+        System.out.println(tim);
+        System.out.println("Age = " + tim.getAge());
+        System.out.println("Pay = " + tim.collectPay());
     }
 }
